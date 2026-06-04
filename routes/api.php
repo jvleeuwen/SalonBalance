7282e1b4d510b33874b02d5ebf562b6ae68e4f75
jvleeuwen <info@jellevl.nl>
2026-06-04 15:19:39 +0200

    cleanup

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\CustomersController;
-use App\Http\Controllers\TreatmentsController;
-use Illuminate\Support\Facades\Route;
-
-Route::apiResource('customers', CustomersController::class);
-Route::apiResource('treatments', TreatmentsController::class);
